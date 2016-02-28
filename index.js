093e16e4cfbc7ebb35a83450c596d3beb334c308
hyperbolic2346 <knobby2346+git@gmail.com>
2016-02-28 18:03:18 -0500

    more debugging to track this kubernetes header thing. api_key is being smashed in the header

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ var options = {
 var security_options = {
     api_key: function (req, authOrSecDef, scopesOrApiKey, cb) {
       try {
+        console.log('scopes ' + scopesOrApiKey);
         console.log(req.headers);
         var decoded = jwt.verify(req.headers.api_key, config.secret);
         if (decoded) {
