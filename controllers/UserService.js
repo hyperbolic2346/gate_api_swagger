82d3f338308ef3498b82887b987947d94918e83d
hyperbolic2346 <knobby2346+git@gmail.com>
2016-02-28 17:33:36 -0500

    adding user service stuff so we can log in

--- controllers/UserService.js
+++ controllers/UserService.js
@@ -1,4 +1,7 @@
 'use strict';
+var mysql  = require('../lib/MysqlLib');
+var config = require('config');
+var jwt    = require('jsonwebtoken');
 
 exports.createUser = function(args, res, next) {
   /**
@@ -18,29 +21,39 @@ exports.loginUser = function(args, res, next) {
   * password (String)
   **/
   
-  
-  var examples = {};
-  examples['application/json'] = {
-  "firstName" : "aeiou",
-  "lastName" : "aeiou",
-  "password" : "aeiou",
-  "userStatus" : 123,
-  "phone" : "aeiou",
-  "id" : 123456789,
-  "display_name" : "aeiou",
-  "email" : "aeiou",
-  "username" : "aeiou"
-};
-  
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  if (!args.username.value || !args.password.value) {
+    res.end('Invalid call!');
+    return;
   }
-  else {
-    res.end();
-  }
-  
-  
+
+  var con = mysql.connect();
+
+  var crypto = require('crypto');
+  var md5_pw_hash = crypto.createHash('md5').update(args.password.value).digest("hex");
+
+  con.query('SELECT * from users WHERE username = "' + args.username.value + '" AND password = "' + md5_pw_hash + '"', function (err, rows, fields) {
+    if (err || rows.length == 0) {
+      res.end('Invalid user information!');
+      return;
+    }
+
+console.log(rows);
+
+    var user_obj = {
+      id: rows[0].user_id,
+      username: rows[0].username,
+      firstName: 'unknown',
+      lastName: 'unknown',
+      email: 'unknown',
+      display_name: rows[0].display_name,
+      auth_token: jwt.sign({ user_id: rows[0].user_id}, config.get('secret'), {
+          expiresIn: 1440 * 60 // expires in 24 hours
+        })
+     };
+
+     res.setHeader('Content-Type', 'application/json');
+     res.end(JSON.stringify(user_obj, null, 2));
+  });
 }
 
 exports.logoutUser = function(args, res, next) {
@@ -49,7 +62,8 @@ exports.logoutUser = function(args, res, next) {
   **/
   // no response value expected for this operation
   
-  
+  // currently don't store the tokens, so can't invalidate them. Is it cool to just let the client
+  // forget the token? 
   res.end();
 }
 
